e13a21b92294e9914c7d7804b4c10bcd55c0ca3d
gythialy <gythialy.koo@gmail.com>
2014-02-10 15:35:49 +0800

    - fixes batch file - update all subscribable list

--- dev/update_all.bat
+++ dev/update_all.bat
@@ -2,9 +2,9 @@
 Rem Combine ChinaList
 ABPUtils.exe -b -i=chinalist/chinalist.txt -o=../adblock.txt
 Rem Combine ChinaList Privacy
-ABPUtils.exe -b -i=chinalist/chinalist.txt -o=../adblock-privacy.txt
+ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../adblock-privacy.txt
 Rem Combine ChinaList Anti Social
-ABPUtils.exe -b -i=chinalist/chinalist.txt -o=../adblock-anti-social.txt
+ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../adblock-anti-social.txt
 Rem Combine ChinaList Lazy
 ABPUtils.exe -m -i=chinalist/ -o=../adblock-lazy.txt -patch
 @pause
