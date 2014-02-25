6dfbc25cbe56c2decc763bc5b0d02b1ea07eeeba
gythialy <gythialy.koo@gmail.com>
2014-02-25 15:34:35 +0800

    add bash script

---
+++ dev/update_all_by_mono.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Combine ChinaList"
+mono ABPUtils.exe -m -o=../adblock.txt -cl
+echo "Combine ChinaList Privacy"
+mono ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../adblock-privacy.txt
+echo "Combine ChinaList Anti Social"
+mono ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../adblock-anti-social.txt
+echo "Combine ChinaList Lazy"
+mono ABPUtils.exe -m -i=chinalist/ -o=../adblock-lazy.txt -patch
+read -n1 -p "Press any key to continue..."
