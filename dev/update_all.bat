b221e6f862cb8f0f9b3053886d7760362fa4fa30
gythialy <gythialy.koo@gmail.com>
2014-03-24 09:49:09 +0800

    - ChinaList rename to ChinaList Lite - ChinaList Lazy rename to ChinaList - redirect list to the NEW - update abputilis config

--- dev/update_all.bat
+++ dev/update_all.bat
@@ -1,10 +1,10 @@
 @echo on
 Rem Combine ChinaList
-ABPUtils.exe -m -o=../adblock.txt -cl
+ABPUtils.exe -m -o=../chinalist-lite.txt -cl
 Rem Combine ChinaList Privacy
-ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../adblock-privacy.txt
+ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../chinalist-privacy.txt
 Rem Combine ChinaList Anti Social
-ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../adblock-anti-social.txt
+ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../chinalist-anti-social.txt
 Rem Combine ChinaList Lazy
-ABPUtils.exe -m -i=chinalist/ -o=../adblock-lazy.txt -patch
+ABPUtils.exe -m -i=chinalist/ -o=../chinalist.txt -patch
 @pause
--- dev/update_all_by_mono.sh
+++ dev/update_all_by_mono.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 echo "Combine ChinaList"
-mono ABPUtils.exe -m -o=../adblock.txt -cl
+mono ABPUtils.exe -m -o=../chinalist-lite -cl
 echo "Combine ChinaList Privacy"
-mono ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../adblock-privacy.txt
+mono ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../chinalist-privacy.txt
 echo "Combine ChinaList Anti Social"
-mono ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../adblock-anti-social.txt
+mono ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../chinalist-anti-social.txt
 echo "Combine ChinaList Lazy"
-mono ABPUtils.exe -m -i=chinalist/ -o=../adblock-lazy.txt -patch
+mono ABPUtils.exe -m -i=chinalist/ -o=../chinalist.txt -patch
 
