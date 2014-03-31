b5a25d9b07ee3a7b2fd35184534b625900b29872
gythialy <gythialy.koo@gmail.com>
2014-03-31 10:54:00 +0800

    rebuild private list

--- dev/update_all.bat
+++ dev/update_all.bat
@@ -1,10 +1,4 @@
 @echo on
-Rem Combine ChinaList
-ABPUtils.exe -m -o=../chinalist-lite.txt -cl
-Rem Combine ChinaList Privacy
-ABPUtils.exe -b -i=chinalist/chinalist-privacy.txt -o=../chinalist-privacy.txt
-Rem Combine ChinaList Anti Social
-ABPUtils.exe -b -i=chinalist/chinalist-anti-social.txt -o=../chinalist-anti-social.txt
-Rem Combine ChinaList Lazy
-ABPUtils.exe -m -i=chinalist/ -o=../chinalist.txt -patch
+Rem Combine Custom List
+ABPUtils.exe -m -i=chinalist/ -o=../my_custom_list.txt -patch
 @pause
