9c8df0da8305d2f0f4ff683f37b8969eeaad6d5e
Goren G <gythialy@users.noreply.github.com>
2016-03-24 03:15:30 +0100

    add grease monkey script for Inoreader

---
+++ scripts/remove_ads_for_inoreader.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name inoreader ads Remover
+// @namespace https://github.com/gythialy/chinalist/
+// @author Gythialy
+// @version 1.0.0
+// @description Remove Inoreader ads for ChinaList
+// @homepage https://github.com/gythialy/chinalist/
+// @updateURL https://github.com/gythialy/chinalist/raw/master/scripts/remove_ads_for_inoreader.user.js
+// @include http*://www.inoreader.com/*
+// @grant none
+// ==/UserScript==
+(function() {
+  $('#sinner_container').remove();
+  $('#reader_pane.reader_pane_sinner').css('padding-right', '0px');
+})();
